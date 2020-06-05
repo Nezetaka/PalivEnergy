01d3924a7aaa20417405e90b7b48928662d38a33
Nezetaka <39554317+Nezetaka@users.noreply.github.com>
2020-06-05 14:07:33 +0300

    profile page added gas table, amount gas table and payment

--- PalivEnergy/pscript.js
+++ PalivEnergy/pscript.js
@@ -49,7 +49,7 @@ function showProfileInfo(tab) {
       sidebar_graf.classList.remove("tab_active");
       break;
     case 4:
-      includeProfileContent.innerHTML = "<p>Розрахунки за гаp</p>";
+      includeProfileContent.innerHTML = '<table class="gas_calc_table"><tr><td class="gastable_title">особовий рахунок</td><td class="gastable_title">дата</td><td class="gastable_title">сума нарахування</td><td class="gastable_title">сума оплат</td><td class="gastable_title">обсяг споживання</td><td class="gastable_title">початок періоду</td><td class="gastable_title">кінець періоду</td></tr><tr><td class="gastable_data">112315989</td><td class="gastable_data">2020 січень</td><td class="gastable_data">1400 грн</td><td class="gastable_data">1300 грн</td><td class="gastable_data">0,20 куб.м.</td><td class="gastable_data">—</td><td class="gastable_data">-100</td></tr><tr><td class="gastable_data">112315989</td><td class="gastable_data">2020 лютий</td><td class="gastable_data">1300 грн</td><td class="gastable_data">1500 грн</td><td class="gastable_data">0,17 куб.м.</td><td class="gastable_data">-100</td><td class="gastable_data">100</td></tr><tr><td class="gastable_data">112315989</td><td class="gastable_data">2020 березень</td><td class="gastable_data">1370 грн</td><td class="gastable_data">1450 грн</td><td class="gastable_data">0,18 куб.м.</td><td class="gastable_data">100</td><td class="gastable_data">180</td></tr><tr><td class="gastable_data">112315989</td><td class="gastable_data">2020 квітень</td><td class="gastable_data">1400 грн</td><td class="gastable_data">1400 грн</td><td class="gastable_data">0,20 куб.м.</td><td class="gastable_data">180</td><td class="gastable_data">180</td></tr></table>';
       sidebar_calc.classList.add("tab_active");
       sidebar_profile.classList.remove("tab_active");
       sidebar_info.classList.remove("tab_active");
@@ -59,7 +59,7 @@ function showProfileInfo(tab) {
       sidebar_graf.classList.remove("tab_active");
       break;
     case 5:
-      includeProfileContent.innerHTML = "<p>Обсяг споживання газу</p>";
+      includeProfileContent.innerHTML = '<table class="gas_amount_table"><tr><td class="gastable_title">дата</td><td class="gastable_title">обсяг споживання</td><td class="gastable_title">сума нарахування</td></tr><tr><td class="gastable_data">08.01.2020</td><td class="gastable_data">12.31 куб. м.</td><td class="gastable_data">1400 грн</td></tr><tr><td class="gastable_data">09.02.2020</td><td class="gastable_data">11.22 куб. м.</td><td class="gastable_data">1300 грн</td></tr><tr><td class="gastable_data">09.03.2020</td><td class="gastable_data">11.44 куб. м.</td><td class="gastable_data">1320 грн</td></tr><tr><td class="gastable_data">08.04.2020</td><td class="gastable_data">13.46 куб. м.</td><td class="gastable_data">1540 грн</td></tr></table>';
       sidebar_amount.classList.add("tab_active");
       sidebar_profile.classList.remove("tab_active");
       sidebar_info.classList.remove("tab_active");
@@ -69,7 +69,7 @@ function showProfileInfo(tab) {
       sidebar_graf.classList.remove("tab_active");
       break;
     case 6:
-      includeProfileContent.innerHTML = "<p>Оплатити за газ через сайт</p>";
+      includeProfileContent.innerHTML = '<form class="profile_payment"><h2 class="profile_payment_title">Виберіть спосіб оплати</h2><input class="profile_payment_checkbox" type="radio" id="onlinePay"         name="contact" value="onlinePay" onclick="showPaymentType()"><label for="onlinePay">оплатити онлайн</label><br><img id="img_payment_type1" class="img_payment_type" src="assets/images/onlinepay.png" alt="pay"><input class="profile_payment_checkbox" type="radio" id="receipPay" name="contact" value="receipPay" onclick="showPaymentType()"><label for="receipPay">оплата по квитанції</label><p id="profile_warning" class="profile_payment_warning">Увага! Змінено банківські реквізити</p><img id="img_payment_type2" class="img_payment_type" src="assets/images/receippay.png" alt="pay"><a href="#" id="profile_print_payment" class="profile_confirm_btn2">роздрукувати</a></form>';
       sidebar_pay.classList.add("tab_active");
       sidebar_profile.classList.remove("tab_active");
       sidebar_info.classList.remove("tab_active");
@@ -90,5 +90,22 @@ function showProfileInfo(tab) {
       break;
     default:
       {}
+  }
+}
+
+function showPaymentType() {
+  if (onlinePay.checked == true) {
+    img_payment_type1.style.display = "block";
+  } else {
+    img_payment_type1.style.display = "none";
+  }
+  if (receipPay.checked == true) {
+    profile_warning.style.display = "block";
+    img_payment_type2.style.display = "block";
+    profile_print_payment.style.display = "inline-block";
+  } else {
+    profile_warning.style.display = "none";
+    img_payment_type2.style.display = "none";
+    profile_print_payment.style.display = "none";
   }
 }
