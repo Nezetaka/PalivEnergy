63eed5bf1bb12ac87b0f383cc6617e730bbf6ce6
Nezetaka <39554317+Nezetaka@users.noreply.github.com>
2020-06-15 13:38:46 +0300

    company page tabs template

--- PalivEnergy/pscript.js
+++ PalivEnergy/pscript.js
@@ -327,3 +327,17 @@ function recoveryNext(step) {
       {}
   }
 }
+
+function openFuel(evt, fuelName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" activetab", "");
+    }
+    document.getElementById(fuelName).style.display = "block";
+    evt.currentTarget.className += " activetab";
+}
